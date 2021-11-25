cfb393271a564fc879eddb5e06da86677b3a3c79
HannahWritesCode <44559340+HannahWritesCode@users.noreply.github.com>
2021-11-25 15:19:37 -0800

    Added models, controllers, views for Event and ApplicationUser

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Scheduler.Models;
 
 namespace Scheduler.Data
 {
@@ -12,5 +13,6 @@ namespace Scheduler.Data
             : base(options)
         {
         }
+        public DbSet<Scheduler.Models.Event> Event { get; set; }
     }
 }
---
+++ Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Models
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ApplicationUser Owner { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
