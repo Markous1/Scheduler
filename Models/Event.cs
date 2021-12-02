27fa456bf53e19af6fa0f1b530027ee9f445f53a
HannahWritesCode <44559340+HannahWritesCode@users.noreply.github.com>
2021-12-02 13:41:51 -0800

    Fixed migrations: added AddingEvent migration. Readded Events and ApplicationUser models, controllers, views.

---
+++ Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Models
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ApplicationUser Owner { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
