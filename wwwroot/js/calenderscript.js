25773d93c80c56e444c9f8ee3e28c3c3e405c156
Bryan-98 <bryanaleman20@hotmail.com>
2021-12-03 03:35:28 -0800

    Added year indicator and placed days on the correct weekdays

--- wwwroot/js/calenderscript.js
+++ wwwroot/js/calenderscript.js
@@ -16,13 +16,6 @@ const months = [
     "December"
 ];
 
-const month30 = [
-    "April", 
-    "June", 
-    "September", 
-    "November"
-];
-
 const dayNames = [
     "Sun",
     "Mon",
@@ -33,41 +26,97 @@ const dayNames = [
     "Sat"
 ];
 
-var currDay = date.getMonth();
-document.getElementById("monthname").innerHTML = months[currDay];
+var currMonth = date.getMonth();
+var currDay = date.getDate();
+var curryear = date.getFullYear();
+var firstDay = new Date(curryear, currMonth, 1).getDay();
+
+document.getElementById("monthname").innerHTML = months[currMonth];
+document.getElementById("year").innerHTML = curryear;
 
 const days = document.querySelector(".days");
 
 let days1 = "";
+
+for (let i = 1; i <= firstDay; i++) {
+    days1 += `<div></div>`;
+}
+
 for (let i = 1; i <= 31; i++) {
     days1 += `<div>${i}</div>`;
-    days.innerHTML = days1;
-  }
+}
+
+days.innerHTML = days1;
 
 const days3 = document.querySelector(".daynames");
 
 let namesOfDays = "";
-for (let i = 0; i <= dayNames.length-1; i++) {
+
+for (let i = 0; i <= dayNames.length - 1; i++) {
     namesOfDays += `<div>${dayNames[i]}</div>`;
     days3.innerHTML = namesOfDays;
 }
 
-function nextCal() {
-    
-    if(currDay == 11){
-        currDay = 0;
-    }else{
-        currDay++;
+var numberOfDays = 0;
+function dayInMoths(month, year) {
+
+    if (month == 1) {
+        if (year % 4 == 0) {
+            numberOfDays = 29
+        } else {
+            numberOfDays = 28;
+        }
     }
-    document.getElementById("monthname").innerHTML = months[currDay];
+    else if (month == 3 || month == 5 || month == 7 || month == 8) {
+        numberOfDays = 30;
+    }
+    else {
+        numberOfDays = 31;
+    }
+}
+
+function renderCalander(firstDays) {
+
+    var newDates = "";
+    while (days.firstChild) {
+        days.removeChild(days.firstChild);
+    }
+
+    dayInMoths(currMonth, curryear);
+    for (let i = 1; i <= firstDays; i++) {
+        newDates += `<div></div>`;
+    }
+
+    for (let i = 1; i <= numberOfDays; i++) {
+        newDates += `<div>${i}</div>`;
+    }
+    days.innerHTML = newDates;
+}
+
+function nextCal() {
+
+    if (currMonth == 11) {
+        currMonth = 0;
+        curryear++;
+    } else {
+        currMonth++;
+    }
+    firstDay = new Date(curryear, currMonth, 1).getDay();
+    renderCalander(firstDay, currMonth);
+    document.getElementById("monthname").innerHTML = months[currMonth];
+    document.getElementById("year").innerHTML = curryear;
 }
 
 function prevCal() {
-    
-    if(currDay == 0){
-        currDay = 11;
-    }else{
-        currDay--;
+
+    if (currMonth == 0) {
+        currMonth = 11;
+        curryear--;
+    } else {
+        currMonth--;
     }
-    document.getElementById("monthname").innerHTML = months[currDay];
+    firstDay = new Date(curryear, currMonth, 1).getDay();
+    renderCalander(firstDay, currMonth);
+    document.getElementById("monthname").innerHTML = months[currMonth];
+    document.getElementById("year").innerHTML = curryear;
 }
