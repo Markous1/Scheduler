73d1e1f6712ef1163f026ac6559f142681f342e5
Alexander Kreuch <alex.kreuch@gmail.com>
2021-12-06 13:14:41 -0800

    Added api-controller class

---
+++ Controllers/ApiController0.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController0 : ControllerBase
+    {
+        #region experiments
+        /* This code is for testing only, and should NOT be included in final product
+         */
+        [Route("test")]
+        public string Foo() { return "hello world!"; }
+        #endregion
+    }
+}
