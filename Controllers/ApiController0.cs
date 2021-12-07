4990ecc6b2125b9d845d1a5e83c2aa0c1a795f97
Alexander Kreuch <alex.kreuch@gmail.com>
2021-12-06 18:21:14 -0800

    prototyping functions

--- Controllers/ApiController0.cs
+++ Controllers/ApiController0.cs
@@ -8,6 +8,7 @@ using Scheduler.Data;
 using Microsoft.AspNetCore.Identity;
 using Scheduler.Models;
 using System.Text.Json;
+using System.Security.Claims;
 
 namespace Scheduler.Controllers
 {
@@ -152,6 +153,9 @@ namespace Scheduler.Controllers
         public class FailReason {
             public const string NEED_LOGIN = "You are not logged in";
             public const string INVALID_JSON = "Your request could not be parsed";
+            public const string NULL_REQ = "The request was null";
+
+            public const string NA = "N/A";
         }
         public class Qtype
         {
@@ -172,40 +176,139 @@ namespace Scheduler.Controllers
                 public string Description { get; set; }
                 public long Start { get; set; } // measured in ticks
                 public long End { get; set; }   // measured in ticks
+                
+                public int? ReturnHash { get; set; }
             };
+
+            public class Event_spec1 : Event_spec // includes owner user-name
+            {
+                public string OwnerUserName { get; set; } // username
+            }
 
             public class GetEvents_requ
             {
                 public long? From { get; set; }
                 public long? To { get; set; }
+
+                public int? ReturnHash { get; set; }
             }
 
             public class GetOneEvent_requ
             {
                 public long Id { get; set; }
+
+                public int? ReturnHash { get; set; }
             }
 
             public class DeleteEvent_requ
             {
                 public long Id { get; set; }
+
+                public int? ReturnHash { get; set; }
             }
 
             public class CreateEvent_requ
             {
                 public Event_spec Spec { get; set; }
+                public int? ReturnHash { get; set; }
             }
 
             public class EditEvent_requ
             {
                 public long Id { get; set; }
                 public Event_spec NewSpec { get; set; }
+
+                public int? ReturnHash { get; set; }
             }
 
 
         }
 
-        
+        public class _Respose {
+            public int ResponseType { get; set; }
+            public bool Success { get; set; }
 
+            public string FailReason { get; set; }
+
+            public string PayLoad { get; set; }
+
+            public int? ReturnHash { get; set; }
+        }
+        private class RequestProcessUtil
+        {
+            #region setup
+            private readonly ApplicationDbContext _context;
+            private readonly UserManager<ApplicationUser> _userManager;
+            private ClaimsPrincipal _user;
+            public RequestProcessUtil(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ClaimsPrincipal user)
+            {
+                _context = context;
+                _userManager = userManager;
+                _user = user;
+            }
+            #endregion
+
+            private ApplicationUser GetAppUser() {
+                Task<ApplicationUser> task = _userManager.GetUserAsync(_user);
+                task.Wait();
+                return task.Result;
+            }
+
+            public _Respose Process_GE(_Request.GetEvents_requ req)
+            {
+                // check if request was null
+                if (req == null) return new _Respose
+                {
+                    Success = false,
+                    ResponseType = Qtype.GET_EVENTS,
+                    FailReason = FailReason.NULL_REQ,
+                    ReturnHash = -1,
+                    PayLoad = ""
+                };
+                // get the current-user
+                ApplicationUser user = GetAppUser();
+                if (user == null) return new _Respose
+                {
+                    Success = false,
+                    ResponseType = Qtype.GET_EVENTS,
+                    FailReason = FailReason.NEED_LOGIN,
+                    ReturnHash = req.ReturnHash,
+                    PayLoad = ""
+                };
+
+                // get events for events 
+                IQueryable<Event> eventSet = _context.Event.Where(e => e.Owner.Id.Equals(user.Id));
+                // filter by date
+                if (req.From != null) eventSet.Where(e => e.StartDateTime.Ticks >= req.From);
+                if(req.To != null) eventSet.Where(e => e.StartDateTime.Ticks <= req.To);
+                // remove sensitive user-info
+                eventSet.Select(e => new _Request.Event_spec1 {
+                    Title = e.Title,
+                    Description = e.Description,
+                    Start = e.StartDateTime.Ticks,
+                    End = e.EndDateTime.Ticks,
+                    OwnerUserName = user.UserName
+                });
+
+                // serialize-result
+                string _payload = JsonSerializer.Serialize(eventSet.ToArray());
+
+                return new _Respose {
+                    Success = true,
+                    ResponseType = Qtype.GET_EVENTS,
+                    FailReason = FailReason.NA,
+                    ReturnHash = req.ReturnHash,
+                    PayLoad = _payload
+                };
+            }
+        }
+
+
+        [Route("GetEvents")]
+        public _Respose GetEvents(_Request.GetEvents_requ req) {
+            var rpu = new RequestProcessUtil(_context, _userManager, User);
+            return rpu.Process_GE(req);
+        }
 
 
     }
